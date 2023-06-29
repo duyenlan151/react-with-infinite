842f1c42c008066a037b629279b47c8e318d07cb
Loan Duyên <43545283+duyenlan151@users.noreply.github.com>
2023-06-29 11:18:21 +0700

    [ADD] add command

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -17,6 +17,7 @@ const App = (): JSX.Element => {
   const dataProducts: Products[] = useMemo(() => {
     if (data?.pages) {
       const newData = data?.pages?.reduce(
+        // for type any, for now I still not resolve it,
         (newArray: any, item: any) => [...newArray, ...item.products],
         []
       );
